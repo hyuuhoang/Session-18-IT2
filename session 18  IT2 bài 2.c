5256de7862b67cabb2a037a220ffd7107a7e8db9
hyuuhoang <Huyhoangzz2k5@gmail.com>
2024-12-20 09:57:26 +0700

    nguyen huy hoang

---
+++ session 18  IT2 bài 2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+    char name[50];
+    int age;
+    char phoneNumber[15];
+};
+
+int main() {
+    struct Student student;
+
+
+    printf("Nhap ten sinh vien: ");
+    fgets(student.name, sizeof(student.name), stdin);
+    student.name[strcspn(student.name, "\n")] = '\0';
+
+    printf("Nhap tuoi sinh vien: ");
+    scanf("%d", &student.age);
+    getchar(); 
+
+    printf("Nhap so dien thoai sinh vien: ");
+    fgets(student.phoneNumber, sizeof(student.phoneNumber), stdin);
+    student.phoneNumber[strcspn(student.phoneNumber, "\n")] = '\0';
+
+    printf("\nThong tin sinh vien:\n");
+    printf("Ten: %s\n", student.name);
+    printf("Tuoi: %d\n", student.age);
+    printf("So dien thoai: %s\n", student.phoneNumber);
+
+    return 0;
+}
+
